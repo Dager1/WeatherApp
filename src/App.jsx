3ffc7d1fb0fb84c531534cf73da0cab597a863ea
f <franklinrohan2002@gmail.com>
2023-05-07 22:55:53 +0530

    chore:tested unicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+
 function App() {
   return (
     <div>
       <h1 className="text-2xl font-thin text-red-300">This is </h1>
       <div className="text-2xl font-bold text-red-600">Helo world</div>
+      <UilReact size="140" color="#61DAFB" />
     </div>
   );
 }
