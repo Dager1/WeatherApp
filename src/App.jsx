0b5bd7c217c5b32ea5f08f365c0e900462b14270
f <franklinrohan2002@gmail.com>
2023-05-07 22:35:44 +0530

    chore:added postcss.config.cjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Helo world</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-thin text-red-300">This is </h1>
+      <div className="text-2xl font-bold text-red-600">Helo world</div>
+    </div>
+  );
 }
 
 export default App;
