a1532973544d3676131c4680af0523611e6aed4e
f <franklinrohan2002@gmail.com>
2023-05-08 23:42:45 +0530

    chore:Created TimeAndLocation components

---
+++ src/components/TimeAndLocation.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TimeAndLocation() {
+  return (
+    <div>
+      <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Tuseday, 31 May 2023 | Local Time 12:46 AM
+        </p>
+      </div>
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-extralight">Berlin , DE </p>
+      </div>
+    </div>
+  );
+}
+
+export default TimeAndLocation;
