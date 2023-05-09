fb22d0bcc6e6ac2388e876d51e66442878c85a44
f <franklinrohan2002@gmail.com>
2023-05-09 13:28:47 +0530

    style:added flex

--- src/components/TemperatureAndDeatails.jsx
+++ src/components/TemperatureAndDeatails.jsx
@@ -20,23 +20,23 @@ function TemperatureAndDeatails() {
         <p className="text-5xl ">34</p>
 
         <div className="flex flex-col space-y-2">
-          <div className="felx font-light text-sm italic justify-center">
+          <div className="flex font-light text-sm italic justify-center">
             <UilTemperature size={18} className="mr-1" />
             Real Fall:
             <span className="font-medium ml-1">32°</span>
           </div>
         </div>
         <div className="flex flex-col space-y-2">
-          <div className="felx font-light text-sm italic justify-center">
+          <div className="flex font-light text-sm italic justify-center">
             <UilTear size={18} className="mr-1" />
             Humidity:
             <span className="font-medium ml-1">65%</span>
           </div>
         </div>
         <div className="flex flex-col space-y-2">
-          <div className="felx font-light text-sm italic justify-center">
+          <div className="flex font-light text-sm italic justify-center">
             <UilWind size={18} className="mr-1" />
-            Real Fall:
+            Wind:
             <span className="font-medium ml-1">11Km</span>
           </div>
         </div>
