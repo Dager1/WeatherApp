106d286de43d2e6c44766f7bdbc3ba56166b5f3b
f <franklinrohan2002@gmail.com>
2023-05-10 21:38:11 +0530

    chore:installed axios

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,16 @@ import Inputs from "./components/Inputs";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDeatails from "./components/TemperatureAndDeatails";
 import Forecast from "./components/Forecast";
+import getWeatherData from "../Services/getWeatherData";
+import getFormattedWeatherData from "../Services/getWeatherData";
 
 function App() {
+  const fecthWeatcher = async () => {
+    const data = await getFormattedWeatherData({ q: "delhi" });
+    console.log(data);
+  };
+  fecthWeatcher();
+
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
